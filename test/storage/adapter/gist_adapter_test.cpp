8cfc50e26b124b3d1155f77157febd0098da66fc
Lorenzo Fontana <fontanalorenzo@me.com>
2015-10-23 00:15:43 +0200

    Gist adapter test file

---
+++ test/storage/adapter/gist_adapter_test.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class GistAdapterTest: public ::testing::Test {
+ protected:
+  GistAdapterTest() {
+
+  }
+  virtual ~GistAdapterTest()() {
+
+  }
+  virtual void SetUp() {
+
+  }
+  virtual void TearDown() {
+
+  }
+};
+
+TEST_F(GistAdapterTest, TestCanWrite) {
+
+}
