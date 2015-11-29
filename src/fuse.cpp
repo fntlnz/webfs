476bdc9f6252ad29a937c4e9c5b23761755db4d8
Lorenzo Fontana <fontanalorenzo@me.com>
2015-11-29 18:51:47 +0100

    FUSE getattr and readdir

--- src/fuse.cpp
+++ src/fuse.cpp
@@ -2,26 +2,66 @@
 #include "node.h"
 #include <iostream>
 
-extern "C" {
-
 #define FUSE_USE_VERSION 26
 
 #include <fuse.h>
-#include <stdio.h>
 #include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <libgen.h>
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
 #endif
 
+webfs::Node *root;
+static struct fuse_operations webfs_fuse_ops;
+
+
 static int getattr_callback(const char *path, struct stat *stbuf) {
-  return -ENOENT;
+  memset(stbuf, 0, sizeof(struct stat));
+  std::string p = std::string(path, strlen(path));
+
+  if (p == "/") {
+    p = "";
+  }
+
+  webfs::Node *node = webfs::findNodeByPath(root, p);
+
+  if (node == NULL) {
+    return -ENOENT;
+  }
+
+  if (node->type == webfs::NodeType::LEAF) {
+    stbuf->st_mode = S_IFREG | 0777;
+    stbuf->st_nlink = 1;
+    stbuf->st_size = 10; // TODO: add size to node
+    return 0;
+  }
+
+  if (node->type == webfs::NodeType::BRANCH) {
+    stbuf->st_mode = S_IFDIR | 0755;
+    stbuf->st_nlink = 2;
+    return 0;
+  }
+
+  return 0;
 }
 
 static int readdir_callback(const char *path, void *buf, fuse_fill_dir_t filler,
-                            off_t offset, struct fuse_file_info *fi) {
+    off_t offset, struct fuse_file_info *fi) {
+  (void) offset;
+  (void) fi;
+  std::string p = std::string(path, strlen(path));
+
+  webfs::Node *node = webfs::findNodeByPath(root, p);
+
+  if (node == NULL) {
+    return -ENOENT;
+  }
+
+  filler(buf, ".", NULL, 0);
+  filler(buf, "..", NULL, 0);
+
+  for(auto n : node->children) {
+    filler(buf, n->name.c_str(), NULL, 0);
+  }
   return 0;
 }
 
@@ -30,7 +70,7 @@ static int open_callback(const char *path, struct fuse_file_info *fi) {
 }
 
 static int read_callback(const char *path, char *buf, size_t size, off_t offset,
-                         struct fuse_file_info *fi) {
+    struct fuse_file_info *fi) {
   return 0;
 }
 
@@ -50,8 +90,9 @@ static int create_callback(const char *path, mode_t mode, struct fuse_file_info 
   return 0;
 }
 
-static fuse_operations get_ops() {
-  struct fuse_operations webfs_fuse_ops;
+extern "C" {
+
+static fuse_operations init_fuse_operations() {
   webfs_fuse_ops.getattr = getattr_callback;
   webfs_fuse_ops.readlink = NULL;
   webfs_fuse_ops.getdir = NULL;
@@ -69,7 +110,6 @@ static fuse_operations get_ops() {
   webfs_fuse_ops.open = open_callback;
   webfs_fuse_ops.read = read_callback;
   webfs_fuse_ops.write = write_callback;
-  webfs_fuse_ops.statfs = NULL;
   webfs_fuse_ops.flush = NULL;
   webfs_fuse_ops.release = NULL;
   webfs_fuse_ops.fsync = NULL;
@@ -88,7 +128,18 @@ static fuse_operations get_ops() {
 } // extern "C"
 
 int mount(int argc, char *argv[]) {
-  fuse_operations ops = get_ops();
+  root = new webfs::Node();
+  root->type = webfs::NodeType::BRANCH;
+  root->name = "";
+
+  auto folder = new webfs::Node();
+  folder->type = webfs::NodeType::BRANCH;
+  folder->name = "folder";
+  root->children.push_back(folder);
+ 
+  init_fuse_operations();
+
+  fuse_operations ops = init_fuse_operations();
   return fuse_main(argc, argv, &ops, NULL);
 }
 
