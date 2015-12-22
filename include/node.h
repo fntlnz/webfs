4b620550dc6b6ccaeef5134cbd17b26d455847b5
Lorenzo Fontana <fontanalorenzo@me.com>
2015-12-22 23:20:09 +0100

    Indentation

--- include/node.h
+++ include/node.h
@@ -12,6 +12,7 @@
 #ifndef WEBFS_NODE_H_
 #define WEBFS_NODE_H_
 namespace webfs {
+
 /**
  * Main Node Data structure used to store local file metadata for mapping with
  * external storage
@@ -23,60 +24,65 @@ class Node {
       LEAF,
     };
 
-  Node(const std::string &n,const Type t):
-    name(n),parent(nullptr),type(t){}
+    Node(const std::string &n,const Type t):
+      name(n),parent(nullptr),type(t){}
 
-  /**
-   * Add a child to the current Node
-   */
-   //TODO how we handle the pointer? who have to free it?
-   //ad a function create Child and do the allocation inside the class?
-  void addChild(Node *child);
+    /**
+     * Add a child to the current Node
+     */
+    //TODO(wise): how we handle the pointer? who have to free it?
+    //ad a function create Child and do the allocation inside the class?
+    void addChild(Node *child);
 
-  /**
-  * Find the node at the provided relativePath (relative to the current node)
-  * starting from the current Node.
-  */
-  //TODO return a const reference?
-  Node *findChild(const std::string &relativePath);
-  //TODO return a const reference?
-  Node *findParent(const std::string &relativePath);
+    /**
+     * Find the node at the provided relativePath (relative to the current node)
+     * starting from the current Node.
+     */
+    //TODO(wise): return a const reference?
+    Node *findChild(const std::string &relativePath);
+    //TODO(wise): return a const reference?
+    Node *findParent(const std::string &relativePath);
 
-  const std::string& getName()const{
-    return name;
-  }
-
-  const Type& getType()const {
-    return type;
-  }
-
-  const std::vector<Node*> getChildren()const{
-    return children;
-  }
-
-  bool operator==(const Node &other)const {
-    bool nodeAreEqual = (name==other.name &&
-    type == other.type &&
-    children.size()==other.children.size());
-
-    if(!nodeAreEqual){
-      return false;
+    const std::string& getName()const{
+      return name;
     }
 
-    //else check the child
-    for(auto i=0u; i< children.size();i++){
-      if((*children[i])!=(*other.children[i]))
-      return false;
-    }//for
-    //all the children are equal
-    return true;
-  }
+    const Type& getType()const {
+      return type;
+    }
 
-  bool operator!=(const Node &other) const{
-    return !(*this == other);
-  }
+    const std::vector<Node*> getChildren()const{
+      return children;
+    }
 
-  File *file;
+    /**
+     * Check if two nodes are equal comparing their properties and children
+     */
+    bool operator==(const Node &other)const {
+      bool nodeAreEqual = (name==other.name &&
+          type == other.type &&
+          children.size()==other.children.size());
+
+      if(!nodeAreEqual){
+        return false;
+      }
+
+      // check if children are equal
+      for(auto i=0u; i< children.size();i++){
+        if ((*children[i]) != (*other.children[i])) {
+          return false;
+        }
+      }// for
+
+      return true;
+    }
+
+    bool operator!=(const Node &other) const{
+      return !(*this == other);
+    }
+
+    File *file;
+
   private:
 
     Node* findInChildren(const std::string &currentName);
