5bed8a88de7938109034ef47d1dfe908617a2caf
wise86Android <wise86android@gmail.com>
2015-12-27 00:10:53 +0100

    add gist service

--- include/storage/storage.h
+++ include/storage/storage.h
@@ -7,7 +7,17 @@ namespace webfs {
 namespace storage {
 class Storage {
   public:
+  /**
+   * upload the data in a new gist
+   * @param buf data to upload
+   * @return remote identifier
+   */
     virtual std::string write(const std::vector<char> &buf) = 0;
+    /**
+   * read the data from a remote gist
+   * @param remoteId remote identifier
+   * @return remote identifier
+   */
     virtual std::vector<char> read(const std::string &remoteId)=0;
     virtual ~Storage(){};
 };
---
+++ test/storage/gist/gistGeneric.cpp
@@ -0,0 +1,49 @@
+#include "gtest/gtest.h"
+#include "../../curlMock/curlMock.h"
+#include "storage/gist.h"
+
+class TestGist : public ::testing::Test{
+
+public:
+
+  virtual void SetUp() {
+    curlMock_init();
+    curlMock_setResponseCode(201);
+  }
+
+  virtual void TearDown() {  }
+
+};
+
+TEST_F(TestGist, checkHeaderCleanUp) {
+  {
+    webfs::storage::Gist gist;
+  }
+  EXPECT_TRUE(curlMock_slist_free_all_isCall());
+}
+
+TEST_F(TestGist, checkCURLCleanUp) {
+  curlMock_enqueuResponse("{\"id\":\"123\"}");
+  {
+    webfs::storage::Gist gist;
+    gist.write({'d'});
+  }
+  EXPECT_TRUE(curlMock_cleanup_isCall());
+}
+
+TEST_F(TestGist, checkHeaderContnet) {
+
+  webfs::storage::Gist gist;
+  EXPECT_TRUE(curlMock_headerHasString("cache-control: no-cache"));
+  EXPECT_TRUE(curlMock_headerHasString("User-Agent: webFS"));
+  EXPECT_TRUE(curlMock_headerHasString("Content-Type: application/json"));
+}
+
+TEST_F(TestGist, checkHeaderContnetWithAuth) {
+  const std::string token("12345667");
+  webfs::storage::Gist gist(token);
+  EXPECT_TRUE(curlMock_headerHasString("cache-control: no-cache"));
+  EXPECT_TRUE(curlMock_headerHasString("User-Agent: webFS"));
+  EXPECT_TRUE(curlMock_headerHasString("Content-Type: application/json"));
+  EXPECT_TRUE(curlMock_headerHasString(token));
+}
